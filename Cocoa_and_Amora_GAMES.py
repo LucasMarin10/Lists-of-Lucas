33ccc0ed97c10d922ac252b80c87c7671e0678d2
LucasMarin10 <84347671+LucasMarin10@users.noreply.github.com>
2021-05-26 15:46:37 -0300

    Add files via upload

---
+++ Cocoa_and_Amora_GAMES.py
@@ -0,0 +1,54 @@
+Games=["Subnautica","Minecraft","Undertale","Snek"]
+print(Games)
+Games.append("Minecraft Dungeons")
+print(Games)
+Games.insert(1,"Catch")
+Games.insert(2,"Game:)")
+print(Games)
+del Games[5]
+print(Games)
+del Games[3]
+del Games[4]
+print(Games)
+GamesPop=Games.pop()
+print(GamesPop)
+print(Games)
+GamesPop=Games.pop()
+print(GamesPop)
+print(Games)
+print(GamesPop)
+Games=["Subnautica","Minecraft","Undertale","Snek"]
+GamesPop=Games.pop(2)
+print(GamesPop)
+print(Games)
+Games.remove("Subnautica")
+print(Games)
+Games=["Subnautica","Minecraft","Undertale","Snek"]
+del Games[0]
+Games=["Subnautica","Subnautica","Minecraft","Undertale","Snek"]
+del Games[0]
+print(Games)
+Games=["Subnautica","Subnautica","Minecraft","Undertale","Snek"]
+Games.remove("Subnautica")
+print(Games)
+Games.remove("Subnautica")
+print(Games)
+Games=["Subnautica","Subnautica","Minecraft","Subnautica","Undertale","Subnautica","Snek"]
+Games.remove("Subnautica")
+print(Games)
+Games.remove("Subnautica")
+print(Games)
+Games.remove("Subnautica")
+print(Games)
+Games.remove("Subnautica")
+print(Games)
+#organizing :)
+print("\tA")
+Games=["Subnautica","Minecraft","Undertale","Snek"]
+print(Games)
+Games.sort()
+print(Games)
+Random=["Control","Amazing","Guy"]
+print(Random)
+Random.sort()
+print(Random)
